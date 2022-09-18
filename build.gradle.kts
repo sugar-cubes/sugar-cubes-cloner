fbb5ddd052e5b94854a5e492bd1cb9c7f5600573
Maxim Butov <mbutov@qulix.com>
2022-09-18 23:59:16 +0300

    Version 0.0.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ apply(from = "licenseFormat.gradle.kts")
 
 allprojects {
     group = "io.github.sugar-cubes"
-    version = "1.0.3"
+    version = "0.0.1"
     description = "Java Reflection Cloner library"
 
     repositories {
