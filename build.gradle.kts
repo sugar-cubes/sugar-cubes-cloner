55d57f3663812133af6981fa4557d88c357b10eb
Maxim Butov <mbutov@qulix.com>
2022-09-27 11:29:46 +0300

    Workflow.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 apply(from = "licenseFormat.gradle.kts")
 
+extra["version"] = file("version.txt").readText()
+
 allprojects {
     group = "io.github.sugar-cubes"
-    version = "0.0.1"
+    version = rootProject.extra["version"] as String
     description = "Java Reflection Cloner library"
 
     repositories {
